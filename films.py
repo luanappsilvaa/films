2a8afa995341f9b284ec1670508c28299efc1e89
luanappsilvaa <168466309+luanappsilvaa@users.noreply.github.com>
2024-06-04 14:33:04 -0300

    films.py

--- films.py
+++ films.py
@@ -2,7 +2,7 @@ import pandas as pd
 import requests
 
 # Carregar o arquivo CSV
-df = pd.read_csv('LetterboxdAllFilms.csv.zip')
+df = pd.read_csv('LetterboxdAllFilms.csv')
 
 # Obter os IDs dos 10 filmes mais assistidos atualmente
 top_10_ids = df['ID'].value_counts().head(10).index.tolist()
