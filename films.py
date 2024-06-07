8266cbbd5a99f9a13e9d6c39937e70c82a1cf640
luanappsilvaa <168466309+luanappsilvaa@users.noreply.github.com>
2024-06-07 10:48:54 -0300

    films.py

--- films.py
+++ films.py
@@ -16,26 +16,5 @@ def get_movie_info(movie_id):
     else:
         return None
  
-top_10_movies_info = []
+st.write(df.head(10))
  
-for movie_id in top_10_ids:
-    movie_info = get_movie_info(movie_id)
-    if movie_info:
-        top_10_movies_info.append(movie_info)
- 
-# Imprimir informações dos 10 filmes mais assistidos
-for movie_info in top_10_movies_info:
-    print("Título:", movie_info['title'])
-    print("Avaliação:", movie_info['averageRating'])
-    print("Resumo:", movie_info['shortDescription'])
-    print("Dados do filme:", movie_info['tags'])
-    print("="*50)
- 
-# Salvar as informações em um arquivo .txt
-with open('top_10_movies_info.txt', 'w', encoding='utf-8') as file:
-    for movie_info in top_10_movies_info:
-        file.write("Título: " + movie_info['title'] + "\n")
-        file.write("Avaliação: " + str(movie_info['averageRating']) + "\n")
-        file.write("Resumo: " + movie_info['shortDescription'] + "\n")
-        file.write("Dados do filme: " + str(movie_info['tags']) + "\n")
-        file.write("="*50 + "\n")
