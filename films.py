78c098b0454cac2dd4fd75a70161dd9732853903
luanappsilvaa <168466309+luanappsilvaa@users.noreply.github.com>
2024-06-14 10:10:17 -0300

    Update films.py

--- films.py
+++ films.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import requests
 import streamlit as st
+import matplotlib.pyplot as plt
+import seaborn as sns
 # Carregar o arquivo CSV
 df = pd.read_csv('ordenado.csv')
 top_10_ids = df['ID'].value_counts().head(10).index.tolist()
@@ -14,6 +16,25 @@ def get_movie_info(movie_id):
     else:
         return None
  
-df = pd.read_csv('country.csv')
+df_country = pd.read_csv('country.csv')
 pais=st.selectbox("países", df ['country'])
 st.write(df[df['country'] == pais])
+
+pais = st.selectbox("Países", df_country['country'].unique())
+st.write(df_paises[df_country['country'] == pais])
+
+# Filtrar os dados pelo país selecionado
+filmes_por_pais = df_country[df_country['country'] == pais]
+
+# Contar a quantidade de filmes por categoria
+contagem_categorias = filmes_por_pais['category'].value_counts()
+
+# Plotar o gráfico
+st.write("Quantidade de filmes por categoria no país selecionado:")
+fig, ax = plt.subplots()
+sns.barplot(x=contagem_categorias.index, y=contagem_categorias.values, ax=ax)
+ax.set_xlabel('Categoria')
+ax.set_ylabel('Quantidade de Filmes')
+ax.set_title('Quantidade de Filmes por Categoria')
+plt.xticks(rotation=45)
+st.pyplot(fig)
