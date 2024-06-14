332a26c5e533d068200a39d8acf3cd992104535f
luanappsilvaa <168466309+luanappsilvaa@users.noreply.github.com>
2024-06-14 09:29:45 -0300

    films.py

--- films.py
+++ films.py
@@ -1,13 +1,9 @@
 import pandas as pd
 import requests
 import streamlit as st
- 
-# Carregar o arquivo CSV
-df = pd.read_csv('ordenado.csv')
- 
-top_10_ids = df['ID'].value_counts().head(10).index.tolist()
- 
-# Função para obter informações de um filme pelo ID
+import matplotlib.pyplot as plt
+
+# Function to get movie info by ID
 def get_movie_info(movie_id):
     url = f"https://api.letterboxd.com/api/v0/films/{movie_id}"
     response = requests.get(url)
@@ -17,7 +13,33 @@ def get_movie_info(movie_id):
     else:
         return None
 
-df = pd.read_csv('country.csv')
-pais=st.selectbox("países", df ['country'])
-st.write(df[df['country'] == pais])
+# Load movie data
+df_movies = pd.read_csv('ordenado.csv')
+
+# Load country data
+df_countries = pd.read_csv('country.csv')
+
+# Create Streamlit app
+st.title('Movie Information by Country')
+
+# Dropdown to select country
+selected_country = st.selectbox("Select a country", df_countries['country'])
+
+# Filter movies by selected country
+filtered_movies = df_movies[df_movies['country'] == selected_country]
+
+# Display filtered movies
+st.write(filtered_movies)
+
+# Count movies per category
+category_counts = filtered_movies['category'].value_counts()
+
+# Plot bar chart
+plt.figure(figsize=(10, 6))
+plt.bar(category_counts.index, category_counts.values)
+plt.xlabel('Category')
+plt.ylabel('Number of Movies')
+plt.title(f'Number of Movies per Category in {selected_country}')
+plt.xticks(rotation=45, ha='right')
+st.pyplot()
 
