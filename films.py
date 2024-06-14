1ecc484165d105f6d8ba95e53b5091b30787f88a
luanappsilvaa <168466309+luanappsilvaa@users.noreply.github.com>
2024-06-14 10:52:09 -0300

    Update films.py

--- films.py
+++ films.py
@@ -27,6 +27,7 @@ lingua = st.selectbox("Língua ",df.sort_values(by='language.1')['language.1'].u
 
 # Filtrar os dados pelo país selecionado
 filmes_por_pais = df[df['country'] == pais]
+fimes_por_genero = filmes_por_pais = df[df['genre.1'] == genero]
 
 # Contar a quantidade de filmes por categoria
 contagem_categorias = filmes_por_pais['genre.1'].value_counts()
