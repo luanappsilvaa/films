6dd6fc636f7bf4e90053b81f49608d2596213666
luanappsilvaa <168466309+luanappsilvaa@users.noreply.github.com>
2024-06-18 14:39:00 -0300

    Update films.py

--- films.py
+++ films.py
@@ -4,18 +4,17 @@ import streamlit as st
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-background_style = """
+st.markdown(
+    """
     <style>
     .stApp {
-        background: url('https://github.com/luanappsilvaa/films/blob/main/sala%20de%20cinema.jpg') no-repeat center center fixed;
+        background: url('https://raw.githubusercontent.com/JuliaFrazao/projeto-A2/main/cortina%20vermelha.jpg') no-repeat center center fixed;
         background-size: cover;
     }
     </style>
-"""
-
-# Use st.markdown para aplicar o estilo de fundo ao seu aplicativo Streamlit
-st.markdown(background_style, unsafe_allow_html=True)
-
+    """,
+    unsafe_allow_html=True)
+    
 # Carregar o arquivo CSV
 df = pd.read_csv('ordenado.csv')
 top_10_ids = df['ID'].value_counts().head(10).index.tolist()
